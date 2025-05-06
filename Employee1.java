558795450f45a5ca33462ac5042cb18ef1d66e31
Ankita707 <patilanku111@gmail.com>
2025-05-06 14:43:10 +0530

    save file

--- Employee1.java
+++ Employee1.java
@@ -2,20 +2,23 @@ public class Employee1{
     String name;
     int id;
     double salary;
-    Employee1(String name,int id,double salary)
+    int age;
+    Employee1(String name,int id,double salary,int age)
     {
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.age=age;
     }
     public void displayInfo()
     {
         System.out.println("Employee id:"+id);
         System.out.println("Employee name:"+name);
         System.out.println("Employee salary:"+salary);
+        System.out.println("Employee age:"+age);
     }
     public static void main(String[] args) {
-        Employee1 emp1= new Employee1("ankita",101,5000.00);
+        Employee1 emp1= new Employee1("ankita",101,5000.00,18);
         emp1.displayInfo();
     }
 }
